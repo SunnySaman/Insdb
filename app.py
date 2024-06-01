adabc5bf403791e1700060bdf56773413d5ce26f
Shengjie <15120004204@163.com>
2024-06-01 13:14:40 +0800

    an index page and tempalte

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+from flask import Flask, render_template
 from flask import url_for
-from flask import Flask
 from markupsafe import escape
 
 # 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：
 app = Flask(__name__)
-
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 # 注册一个处理函数，这个函数是处理某个请求的处理函数，Flask 官方把它叫做视图函数（view funciton），你可以理解为“请求处理函数”
 # 所谓的“注册”，就是给这个函数戴上一个装饰器帽子。我们使用 app.route() 装饰器来为这个函数绑定对应的 URL，当用户在浏览器访问这个 URL 的时候，就会触发这个函数，获取返回值，并把返回值显示到浏览器窗口：
@@ -14,16 +26,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
+def index():
+    # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
 
 # 之所以把传入 app.route 装饰器的参数称为 URL 规则，是因为我们也可以在 URL 里定义变量部分。比如下面这个视图函数会处理所有类似 /user/<name> 的请求：
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User page'
-
-
 # 注意 用户输入的数据会包含恶意代码，所以不能直接作为响应返回，需要使用 MarkupSafe（Flask 的依赖之一）提供的 escape() 函数对 name 变量进行转义处理，比如把 < 转换成 &lt;。这样在返回响应时浏览器就不会把它们当做代码执行。
 @app.route('/user/<name>')
 def user_page(name):
@@ -45,7 +52,7 @@ def user_page(name):
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    print(url_for('index'))  # 生成 hello 视图函数对应的 URL，将会输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
     print(url_for('user_page', name='peter'))  # 输出：/user/peter
@@ -53,3 +60,4 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
